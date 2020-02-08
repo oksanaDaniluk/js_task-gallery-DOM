dd0a95782737607e2d4e82136f561c63e9db088b
oksanaDaniluk <oksana.daniluk@ukr.net>
2020-02-09 00:10:59 +0200

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const thumbs = document.querySelectorAll('.gallery__thumb');
+const largeImg = document.querySelector('#largeImg');
+let current = 0;
+const count = thumbs.length;
+
+[].forEach.call(thumbs, function(thumb, n) {
+  thumb.addEventListener('click', function() {
+    event.preventDefault();
+    showPhoto(n);
+  });
+});
+
+function showPhoto(index) {
+  const src = thumbs[index].closest('a').getAttribute('href');
+
+  largeImg.setAttribute('src', src);
+  current = index;
+}
+
+largeImg.addEventListener('click', function() {
+  showPhoto((current + 1) % count);
+});
